5d9b9b96ddaa895d107a6edd47014dbcebdb9fd7
Pierre PACI <pierre.paci@gmail.com>
2017-04-16 17:41:33 +0200

    new: Arg Parser

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 # coding=UTF8
-from yast import *
+import argparse
+from yast import process_file
 
 if __name__ == '__main__':
-    process_file(path_to_file="Ressources/test_input.txt",
-                 path_to_dict="Ressources/test_dict",
-                 path_output_file="Ressources/test_output.csv",
-                 separator=" |\.|,")
+    parser = argparse.ArgumentParser(description='Yet Another Sequence Translator')
+    parser.add_argument('--input', metavar="input.txt", type=str, help='Path to file to transcode', required=True)
+    parser.add_argument('--output', metavar="output.txt", type=str, help='Path to output file', required=True)
+    parser.add_argument('--mapping', metavar="mapping.vec", type=str, help='Path to mapping file', required=True)
+    parser.add_argument('--separator', metavar="\ |\.|\,", type=str,
+                        help='regular expression used to split the input sequence', required=False, default="\ ")
+    args = parser.parse_args()
+    process_file(path_to_file=args.input,
+                 path_to_dict=args.mapping,
+                 path_output_file=args.output,
+                 separator=args.separator)
