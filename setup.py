bdb7daa49fca0b36f4a0648a1812f621b893a683
pierre paci <pierre.paci@gmail.com>
2017-05-01 19:33:42 +0200

    update: bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='yase',
-    version='1.0',
+    version='1.0.1',
     url='https://github.com/PPACI/yase',
     license='MIT',
     author='Pierre Paci',
